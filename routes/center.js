2017513b5b6e11ac2cadbf70c4b2af4f481951ec
Gal Sharir <gal.sharir@post.idc.ac.il>
2015-08-15 10:58:12 +0300

    Organised the display
    - Added a home bar to layout
    - fixed the structure of routes

--- routes/center.js
+++ routes/center.js
@@ -4,4 +4,26 @@
 //General Setting
 var express = require('express');
 var router = express.Router();
+
+// middleware specific to this router
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
+router.get('/', function(req, res, next) {
+    res.render('center', {"shippingDay": 0, "numberOfOrders": 230});
+});
+
+router.get('/stock', function(req, res, next) {
+    // get summery of stock
+
+    //getSummeryOfStock(items);
+    var items = [
+        {"Name":"bananas", "InStock":12, "Ordered":3},
+        {"Name":"tomatos", "InStock":34, "Ordered":23}
+    ];
+    res.render('stock', {itemlist: items});
+});
+
 module.exports = router;
